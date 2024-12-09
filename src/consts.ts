3889f6c6e2eac0808e5549bba9622dec95d4875a
cuiruilong <crl_228@163.com>
2024-12-09 17:37:16 +0800

    feat: change site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'To Be A Coder';
+export const SITE_DESCRIPTION = 'Stay Hungry, Stay Foolish';
